3f7f3bde409c370beb11a69e75d705315cebf1a8
Sonmor <romankuz@gmail.com>
2023-06-01 08:43:17 +0300

    Program.cs: create ActorSystem and actors

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -11,23 +11,27 @@ namespace WinTail
         static void Main(string[] args)
         {
             // initialize MyActorSystem
-            // YOU NEED TO FILL IN HERE
+            // YOU NEED TO FILL IN HEREs
+            MyActorSystem = ActorSystem.Create("MyActorSystem");
 
             PrintInstructions();
 
             // time to make your first actors!
             //YOU NEED TO FILL IN HERE
             // make consoleWriterActor using these props: Props.Create(() => new ConsoleWriterActor())
+            var consoleWriterActorProps = Props.Create(() => new ConsoleWriterActor());
+            var consoleWriterActor = MyActorSystem.ActorOf(consoleWriterActorProps);
             // make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
-
-
+            var consoleReaderActorProps = Props.Create(() => new ConsoleReaderActor(consoleWriterActor));
+            var consoleReaderActor = MyActorSystem.ActorOf(consoleReaderActorProps);
             // tell console reader to begin
             //YOU NEED TO FILL IN HERE
+            consoleReaderActor.Tell("start");
 
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.WhenTerminated.Wait();
         }
-
+        
         private static void PrintInstructions()
         {
             Console.WriteLine("Write whatever you want into the console!");
